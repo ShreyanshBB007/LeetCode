2dcdcc69a3a78f3a929a9b1a514b83ba065b382b
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2025-08-16 09:24:35 +0530

    Time: 6 ms (56.31%) | Memory: 13.2 MB (55.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.length();
-        int value = 0;
-        for (int i = 0; i < n; i++) {
-            if (s.at(i) == 'M') {
-                value += 1000;
-            }
-            if (s.at(i) == 'D') {
-                value += 500;
-            }
-            if (s.at(i) == 'C') {
-
-                if (i != n - 1 && (s.at(i + 1) == 'M' || s.at(i + 1) == 'D')) {
-                    value -= 100;
-                }
-
-                else value += 100;
-            }
-            if (s.at(i) == 'L') {
-                value += 50;
-            }
-            if (s.at(i) == 'X') {
-
-                if (i != n - 1 && (s.at(i + 1) == 'L' || s.at(i + 1) == 'C')) {
-                    value -= 10;
-                }
-
-                else value += 10;
-            }
-            if (s.at(i) == 'V') {
-                value += 5;
-            }
-            if (s.at(i) == 'I') {
-
-                if (i != n - 1 && (s.at(i + 1) == 'V' || s.at(i + 1) == 'X')) {
-                    value -= 1;
-                }
-
-                else value += 1;
+        unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int total = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && values[s[i]] < values[s[i + 1]]) {
+                total -= values[s[i]];
+            } 
+            else {
+                total += values[s[i]];
             }
         }
-        return value;
+        
+        return total;
     }
 };
