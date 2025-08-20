cfcf748a76d7f12be30887feed42de19189712c6
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2025-08-21 00:15:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.51%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int ipot(int n) {
-        if (n == 1) {
-            return 1;
-        } else if (n <= 0 || n % 2 != 0) {
-            return 0;
-        } else
-            return ipot(n / 2);
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return 0;
+        while(n>1){
+            if(n%2!=0)return 0;
+            n=n/2;
+        }
+        return 1;
     }
-    bool isPowerOfTwo(int n) { return ipot(n); }
 };
