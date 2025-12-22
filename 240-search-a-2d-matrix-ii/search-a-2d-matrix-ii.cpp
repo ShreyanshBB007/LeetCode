b91ae2e66a0f5fef54b9de84800b406a264b24f9
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2025-12-22 11:01:08 +0530

    Time: 94 ms (16.18%) | Memory: 18.7 MB (67.56%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0;
+        int high = n - 1;
+
+        for(int i = 0; i<m; i++){
+            low = 0;
+            high = n - 1;
+            while(low<=high){
+                int mid = low + (high - low)/2;
+
+                if(matrix[i][mid]==target){
+                    return true;
+                }
+                else if(matrix[i][mid]>target){
+                    high = mid - 1;
+                }
+                else if(matrix[i][mid]<target){
+                    low = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+};
