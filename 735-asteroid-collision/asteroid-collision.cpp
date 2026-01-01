c4993fda19da51dc0886f434fe9023be3bceeb75
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-01 20:43:04 +0530

    Time: 3 ms (29.09%) | Memory: 22.2 MB (38.20%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -3,46 +3,19 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
         vector<int> ans;
-        st.push(asteroids[0]);
-        int i = 1;
-
-        while (i < asteroids.size() && !st.empty()) {
-            int s = st.top();
-            if (asteroids[i] > 0) {
+        for(int i = 0; i<asteroids.size(); i++){
+            if(asteroids[i]>0){
                 st.push(asteroids[i]);
-                i++;
-            } else if (asteroids[i] < 0) {
-                while (true) {
-                    if (s < 0 && asteroids[i] < 0) {
-                        st.push(asteroids[i]);
-                        s = st.top();
-                        i++;
-                        break;
-                    } else if (s > 0) {
-                        if (s < abs(asteroids[i])) {
-                            st.pop();
-                        } else if (s == abs(asteroids[i])) {
-                            st.pop();
-                            i++;
-                            if(st.empty()&&i!=asteroids.size()){
-                                st.push(asteroids[i]);
-                                s = st.top();
-                                i++;
-                            }
-                            break;
-                        } else {
-                            i++;
-                            break;
-                        }
-
-                        if (st.empty()) {
-                            st.push(asteroids[i]);
-                            s = st.top();
-                            i++;
-                            break;
-                        } else
-                            s = st.top();
-                    }
+            }
+            else{
+                while(!st.empty()&&st.top()>0&&st.top()<abs(asteroids[i])){
+                    st.pop();
+                }
+                if(!st.empty()&&st.top()==abs(asteroids[i])){
+                    st.pop();
+                }
+                else if(st.empty() || st.top() < 0){
+                    st.push(asteroids[i]);
                 }
             }
         }
