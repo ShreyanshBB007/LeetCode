9e69e101d1d36b49ad4e71d48c562cac09a1970d
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-02 01:32:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (29.21%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int sum = 0;
+        for (int i = 0; i < num.size(); i++) {
+            if (i % 2 == 0) {
+                sum += (num[i]-'0');
+            } else
+                sum -= (num[i]-'0');
+        }
+        if (sum == 0) {
+            return true;
+        } else
+            return false;
+    }
+};
