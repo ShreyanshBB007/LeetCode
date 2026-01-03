225fb133f2efa1dab755321b400bf76db998a113
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-03 19:39:43 +0530

    Time: 11 ms (93.49%) | Memory: 81.5 MB (58.47%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int area = 0;
-        int n;
-        int minn;
         stack<int> st;
-        int s = heights.size();
-        vector<int> left(s, 0);
-        vector<int> right(s, 0);
+        int ans = 0;
+        heights.push_back(0);
 
-        for (int i = s - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int height = heights[st.top()];
                 st.pop();
-            }
-            if (!st.empty() && heights[st.top()] < heights[i]) {
-                right[i] = st.top() - 1;
-            }
-            if (st.empty()) {
-                right[i] = s - 1;
+                int width = st.empty() ? i : i - st.top() - 1;
+                ans = max(ans, height * width);
             }
             st.push(i);
         }
-        st = {};
-        for (int i = 0; i < s; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            if (!st.empty() && heights[st.top()] < heights[i]) {
-                left[i] = st.top() + 1;
-            }
-            if (st.empty()) {
-                left[i] = 0;
-            }
-            st.push(i);
-        }
-
-        for (int i = 0; i < s; i++) {
-            n = (right[i] - left[i] + 1);
-            area = max(area, n * heights[i]);
-        }
-
-        return area;
+        return ans;
     }
 };
