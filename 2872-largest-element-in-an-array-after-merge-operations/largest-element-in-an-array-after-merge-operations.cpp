afd08c9fef5ea63f58fecdcc39b49ce26d9dee14
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-13 22:21:22 +0530

    Time: 4 ms (24.18%) | Memory: 110.6 MB (71.98%) - LeetSync

--- 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.cpp
+++ 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     long long maxArrayValue(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
+        long long sum = 0;
+        for(int i= nums.size()-1 ; i>=0 ; i--){
+            if(nums[i] <= sum) sum += nums[i];
+            else sum = nums[i];
         }
-        long long curr = nums[nums.size()-1];
-        long long maxx = *max_element(nums.begin(), nums.end());
-        for (int i = nums.size()-2; i >=0; i--) {
-            if(nums[i]<=curr){
-                curr += nums[i];
-                maxx = max(curr,maxx);
-            }
-            else{
-                curr = nums[i];
-            }
-        }
-        return maxx;
+        return sum;
     }
 };
