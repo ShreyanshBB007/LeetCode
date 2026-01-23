3799f93c19204abf9461c9e6f2cd3721766af172
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-23 23:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (79.39%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,27 +1,34 @@
+#define ll unsigned long long
+#define mod 1000000007
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        for(int i = 0; i < haystack.size(); i++){
-            if(haystack[i]==needle[0]){
-                int j  = 1;
-                int k = i+1;
-                int flag = 0;
-                while(j<needle.size()){
-                    if(haystack[k]!=needle[j]){
-                        flag = 1;
-                    }
-                    else {
-                        k++;
-                        j++;
-                    }
-                    if(flag == 1){
-                        break;
-                    }
-                }
-                if(flag==0){
-                    return i;
-                }
-            }
+ll base;
+
+    ll expo(ll a, ll b ) {
+        if ( b == 0 ) return 1;
+        ll half = expo(a, b/2);
+        if ( b & 1 ) return (half % mod * half % mod * a) % mod;
+        else return half * half % mod ;
+    }
+
+    ll calHash(string &s, int j) {
+        ll hash = 0;
+        for ( int i=0; i<=j; i++ ) {
+            hash = (hash * base + (s[i] - 'a')) % mod ; 
+        }
+        return hash;
+    }
+
+    int strStr(string s, string t) {
+        int m = s.size(), n = t.size();
+        base = 11;
+        ll thash = calHash(t, n-1);
+
+        ll shash = calHash(s, n-1);
+        for ( int i=0; i<=m-n; i++ ) {
+            if ( thash == shash ) return i;
+            shash = (shash - (( (s[i] - 'a') * expo(base, n-1) ) % mod ) + mod) % mod ;
+            shash = (shash * base + (s[i+n] - 'a') ) % mod ;
         }
         return -1;
     }
