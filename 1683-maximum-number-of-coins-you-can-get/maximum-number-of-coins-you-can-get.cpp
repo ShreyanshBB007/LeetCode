689945cb192417e3a9583e72a06d32aa8d47db9b
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-27 15:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 58.4 MB (12.78%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
-        sort(piles.begin(), piles.end());
         int sum = 0;
         int n = piles.size();
-        for(int i = 1; i<=(n/3); i++){
-            sum+=piles[n-2*i];
+        int steps = n / 3;
+        int maxx = *max_element(piles.begin(), piles.end());
+        vector<int> freq(maxx + 1, 0);
+        for (int i = 0; i < n; i++) {
+            freq[piles[i]]++;
+        }
+        int low = 0;
+        int high = maxx;
+        while (steps > 0) {
+            while (high >= 0 && freq[high] == 0) {
+                high--;
+            }
+            freq[high]--;
+            while (high >= 0 && freq[high] == 0) {
+                high--;
+            }
+            sum += high;
+            freq[high]--;
+            while (low < high && freq[low] == 0) {
+                low++;
+            }
+            freq[low]--;
+            steps--;
         }
         return sum;
     }
