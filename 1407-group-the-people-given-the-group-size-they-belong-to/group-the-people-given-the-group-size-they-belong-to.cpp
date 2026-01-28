d6e47a2fe2580c24fd9971919b137767dcaf5964
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-01-28 21:47:32 +0530

    Time: 4 ms (54.44%) | Memory: 17.4 MB (39.73%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        int maxx = *max_element(groupSizes.begin(), groupSizes.end());
-        vector<vector<int>> vec(maxx+1);
-
-        for(int i = 0; i<groupSizes.size(); i++){
-            vec[groupSizes[i]].push_back(i);
-        }
-        int i = 0;
-        while (i < vec.size()){
-            if(vec[i].empty()){
-                vec.erase(vec.begin()+i);
-                continue;
+        int n = groupSizes.size();
+        vector<vector<int>> ans;
+        vector<vector<int>> mp(n+1);
+        for(int i = 0; i < n; i++){
+            mp[groupSizes[i]].push_back(i);
+            if(mp[groupSizes[i]].size() == groupSizes[i]){
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]] = {};
             }
-            while(!vec[i].empty()&&vec[i].size()>groupSizes[vec[i][0]]){
-                vector<int> temp1;
-                vector<int> temp2;
-                for(int j = 0; j<vec[i].size(); j++){
-                    if(j<groupSizes[vec[i][0]]){
-                        temp1.push_back(vec[i][j]);
-                    }
-                    else temp2.push_back(vec[i][j]);
-                }
-                vec.erase(vec.begin()+i);
-                vec.insert(vec.begin()+i,temp2);
-                vec.insert(vec.begin()+i,temp1);
-
-            }
-            i++;
         }
-        return vec;
+        return ans;
     }
 };
