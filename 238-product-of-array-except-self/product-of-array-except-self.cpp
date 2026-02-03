80ed24da969270dfc048664e00ca3ab53f64c67f
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-02-03 16:26:51 +0530

    Time: 4 ms (21.17%) | Memory: 40.3 MB (43.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n  = nums.size();
-        vector<int> prefixprod(n,1);
-        vector<int> suffixprod(n,1);
-        int prod = 1;
-        prefixprod[0]=1;
-        suffixprod[n-1]=1;
-        vector<int> ans;
-        int flag = 1;          
-        int zero = 0;
+        int n = nums.size();
+        vector<int> res(n,1);
 
-        // for(int i = 0; i < n; i++){
-        //     if(nums[i]==0){
-        //         flag = 0;
-        //         zero = i;
-        //     }
-        //     else prod*=nums[i];
-        // }
-        // if(!flag){
-        //     ans[zero]=prod;
-        // }
-        for(int i = 1; i <n; i++){
-            prefixprod[i]=prefixprod[i-1]*nums[i-1];
+        int left = 1;
+        for(int i=0; i<n; i++){
+            res[i] = left;
+            left = left * nums[i];
         }
-        for(int i = n-2; i >=0; i--){
-            suffixprod[i]=suffixprod[i+1]*nums[i+1];
+
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            res[i] = res[i] * right;
+            right = right * nums[i];
         }
-        for(int i = 0; i<n; i++){
-            ans.push_back(prefixprod[i]*suffixprod[i]);
-        }
-        return ans;
-        
+
+        return res;
     }
 };
