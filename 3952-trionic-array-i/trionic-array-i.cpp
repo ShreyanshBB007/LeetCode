589b2e360477b3697fc56e6f69e9fc710994c32c
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-02-04 08:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (35.37%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-    // Checks if the array follows a "trionic" pattern:
-    // strictly increasing → strictly decreasing → strictly increasing
-    // Time: O(n), Space: O(1)
     bool isTrionic(vector<int>& nums) {
         int flag1 = 0, flag2 = 0, flag3 = 0;
         int i = 0, j = 1;
         int n = nums.size();
 
-        // Phase 1: strictly increasing
         while (j < n && nums[i] < nums[j]) {
             flag1 = 1;
             i = j;
@@ -17,7 +13,6 @@ public:
         if (j >= n || nums[i] == nums[j])
             return false;
 
-        // Phase 2: strictly decreasing
         while (j < n && nums[i] > nums[j]) {
             flag2 = 1;
             i = j;
@@ -26,7 +21,6 @@ public:
         if (j >= n || nums[i] == nums[j])
             return false;
 
-        // Phase 3: strictly increasing
         while (j < n && nums[i] < nums[j]) {
             flag3 = 1;
             i = j;
@@ -36,5 +30,6 @@ public:
             return false;
 
         return (flag1 && flag2 && flag3);
+            
     }
 };
