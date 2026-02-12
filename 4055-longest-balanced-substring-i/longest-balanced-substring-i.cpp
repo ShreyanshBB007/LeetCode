26909ee00e062ebe582f92a0c58674e4eec50e88
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-02-12 22:15:46 +0530

    Time: 23 ms (99.59%) | Memory: 12.4 MB (91.26%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -2,25 +2,36 @@ class Solution {
 public:
     int longestBalanced(string s) {
         int n = s.size();
-        int res = 0;
-        vector<int> cnt(26);
-        for (int i = 0; i < n; i++) {
-            fill(cnt.begin(), cnt.end(), 0);
-            for (int j = i; j < n; j++) {
-                bool flag = true;
-                int c = s[j] - 'a';
-                cnt[c]++;
-                for (auto x : cnt) {
-                    if (x > 0 && x != cnt[c]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    res = max(res, j - i + 1);
-                }
+
+        // Transform char -> int
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) 
+            a[i] = s[i] - 'a';
+
+        int result = 0;
+        for (int l = 0; l < n; ++l) {
+            // Early exit, can't be bigger
+            if (n - l <= result) break;
+
+            int cnt[26] = {0};          // Counts of every char
+            int uniq = 0, maxfreq = 0;  // Number of uniq chars and maximum frequency
+            for (int r = l; r < n; ++r) {
+                int i = a[r];
+
+                // There was no this char before => one more uniq
+                if (cnt[i] == 0) ++uniq;
+
+                ++cnt[i];
+                // Update max frequency
+                if (cnt[i] > maxfreq) 
+                    maxfreq = cnt[i];
+
+                // Check if all uniq chars have maxfreq frequency then update the result
+                int cur = r - l + 1;
+                if (uniq * maxfreq == cur && cur > result)
+                    result = cur;
             }
         }
-        return res;
+        return result;
     }
 };
