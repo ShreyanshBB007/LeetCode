935b7393000d872c4e494a65e51beab97a86fadc
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-03-11 22:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (4.02%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -4,21 +4,16 @@ public:
         if (n == 0) {
             return 1;
         }
-
         int temp = n;
         string str = "";
-
         while (temp > 0) {
             int rem = temp % 2;
             temp /= 2;
             str += to_string(rem);
         }
-        
-        reverse(str.begin(), str.end());
         int ans = 0;
         int t = 0;
-
-        for(int i = str.size()-1; i>= 0; i--) {
+        for(int i = 0; i<str.size(); i++) {
             if(str[i] == '0'){
                 ans += pow(2,t);
             }
