fa72097763b67fa61c9b0e2fb4654f063ab3fff7
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-03-12 21:26:10 +0530

    Time: 3 ms (14.29%) | Memory: 9.1 MB (80.88%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        while (i < s.size()) {
-            if (s[i] != ' ') {
-                s = s.substr(i, s.size() - i);
-                break;
-            } else
-                i++;
-        }
-        if (s[0] >= 'A' && s[0] <= 'z') {
-            return 0;
-        }
-        int sign = 1;
-        if (s[0] == '-') {
-            sign = -1;
-            s = s.substr(1, s.size() - 1);
-        } else if (s[0] == '+') {
-            s = s.substr(1, s.size() - 1);
-        }
-        i = 0;
-        while (i < s.size()) {
-            if (s[i] != '0') {
-                s = s.substr(i, s.size() - i);
-                break;
-            } else
-                i++;
-        }
-        i = 0;
-        long long ans = 0;
-        while (i < s.size() && (s[i] >= '0' && s[i] <= '9')) {
-            ans = ans * 10 + (s[i] - '0');
-            if (sign * ans > INT_MAX) return INT_MAX;
-            if (sign * ans < INT_MIN) return INT_MIN;
+        int i = 0, sign = 1;
+        long res = 0;
+        while (i < s.size() && s[i] == ' ') i++;
+        if (i == s.size()) return 0;
+        if (s[i] == '-') { sign = -1; i++; }
+        else if (s[i] == '+') i++;
+        while (i < s.size() && isdigit(s[i])) {
+            res = res * 10 + (s[i] - '0');
+            if (sign * res > INT_MAX) return INT_MAX;
+            if (sign * res < INT_MIN) return INT_MIN;
             i++;
         }
-        return sign*ans;
+        return (int)(sign * res);
     }
 };
