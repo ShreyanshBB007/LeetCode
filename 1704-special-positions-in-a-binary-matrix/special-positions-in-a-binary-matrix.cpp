28a4be1c24927fe356b595da563d184ed535225c
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-04-14 19:49:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (21.15%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        int ans = 0;
-        for(int row = 0; row < rows; row++){
-            for(int col = 0; col < cols; col++){
-                if(mat[row][col]==1){
-                    int colsum = 0;
-                    int rowsum = 0;
-                    for(int i = 0; i < rows; i++){
-                        rowsum+=mat[i][col];
-                    }
-                    for(int i = 0; i < cols; i++){
-                        colsum+=mat[row][i];
-                    }
-                    if(rowsum==1&&colsum==1){
-                        ans++;
-                    }
+        int m = mat.size(), n = mat[0].size();
+        vector<int> row(m, 0), col(n, 0);
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
                 }
-            }
-        }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                    ans++;
+
         return ans;
     }
 };
