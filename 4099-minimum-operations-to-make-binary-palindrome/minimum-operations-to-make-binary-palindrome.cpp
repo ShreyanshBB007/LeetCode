8d0bae1832603c0c45a0ba3263eb5923abae758d
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-04-18 13:23:37 +0530

    Time: 391 ms (49.11%) | Memory: 43.7 MB (93.99%) - LeetSync

--- 4099-minimum-operations-to-make-binary-palindrome/minimum-operations-to-make-binary-palindrome.cpp
+++ 4099-minimum-operations-to-make-binary-palindrome/minimum-operations-to-make-binary-palindrome.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool isPalindrome(string str){
+    bool isPalindrome(string str) {
         string rev = str;
         reverse(rev.begin(), rev.end());
-        if(str == rev){
+        if (str == rev) {
             return true;
-        }
-        else return false;
+        } else
+            return false;
     }
-    string getbin(int n){
+    string getbin(int n) {
         int base = 1;
         string str = "";
-        while(n>0){
-            if(n%2){
-                str+='0';
-            }
-            else str+='1';
+        while (n > 0) {
+            if (n % 2) {
+                str += '0';
+            } else
+                str += '1';
             n /= 2;
         }
         reverse(str.begin(), str.end());
@@ -23,22 +23,26 @@ public:
     }
     vector<int> minOperations(vector<int>& nums) {
         vector<int> ans(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            int x = nums[i];
 
-        for(int i = 0; i < nums.size(); i++){
-            int ops = 0;
-            if(!isPalindrome(getbin(nums[i]))){
-                ops++;
-                int nums1 = nums[i] + 1;
-                int nums2 = nums[i] - 1;
-                while(!isPalindrome(getbin(nums1)) && !isPalindrome(getbin(nums2))){
-                    nums1++;
-                    if(nums2>0){
-                        nums2--;
-                    }
-                    ops++;
-                }
+            if (isPalindrome(getbin(x))) {
+                ans[i] = 0;
+                continue;
             }
-            ans[i] = ops;
+
+            int d = 1;
+            while (true) {
+                if (isPalindrome(getbin(x + d))) {
+                    ans[i] = d;
+                    break;
+                }
+                if (x - d >= 0 && isPalindrome(getbin(x - d))) {
+                    ans[i] = d;
+                    break;
+                }
+                d++;
+            }
         }
         return ans;
     }
