a3a465a0355babe4b82371a4c2cd38b900f77e29
Shreyansh Bharat Bhagwat <shreyanshbhagwat007@gmail.com>
2026-04-18 08:02:59 +0530

    Time: 62 ms (20.86%) | Memory: 90.9 MB (44.64%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(int x : nums){
+            mpp[x]++;
+            if(mpp[x] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
